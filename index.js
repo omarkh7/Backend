20a3c9ead6f03e2f5839fb417a243bd69c4e2524
Assala <ismailassala1@gmail.com>
2023-01-23 11:45:05 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const dotenv=require("dotenv")
+dotenv.config();
+const mongoose = require("mongoose");
+const connection = require("./configure/db.js");
+const express = require("express");
+const app = express();
+
+mongoose.set("strictQuery", true);
+
+connection();
+
+const conn = mongoose.connection;
+app.use(express.json());
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(`Listening on port ${port}...`));
+
