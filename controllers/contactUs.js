ed1786433f9b2a9bd7f1629127a44c678a9212eb
hasan <hasankanj2000@gmail.com>
2023-01-25 11:40:39 +0300

    cleaning ContactUs.js code

--- controllers/contactUs.js
+++ controllers/contactUs.js
@@ -7,19 +7,6 @@ const getallcontactinfo = async (req, res) => {
   res.status(200).json(contacts);
 };
 
-// get a single contact info
-// const getsinglecontactinfo = async (req, res) => {
-//   const { id } = req.params
-
-//   if (!mongoose.Types.ObjectId.isValid(id)) {
-//     return res.status(404).json({error: 'No such workout'})
-//   }
-
-//   const contactUs = await contactUs.findById(id)
-
-//   if (!contactUs) {
-//     return res.status(404).json({error: 'No such workout'})
-//   }
 
 
 //add new contact
@@ -43,26 +30,7 @@ catch(err){
 }
 
 
-
-//   res.status(200).json(contactUs)
-// }
-
-// create a new contactInfo
-// const createform = async (req, res) => {
-//   console.log("body ", req.body);
-//   const { name, email, subject, message } = req.body;
-
-//   // add to the database
-//   try {
-//     const new_contactUs = await contactUs.create({name, email, subject, message})
-//     res.status(200).json(new_contactUs);
-//   } catch (error) {
-//     res.status(400).json({ error: error.message })
-//   }
-// }
-
-////////////////////////////////////////////////////////////////
-// delete a contact
+// delete a contact by id
 const deletecontactdetail = async (req, res) => {
   
 
